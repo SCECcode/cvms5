dbb28ba00a9142022ad4e6d16a1c531fac7fd76f
Mei-Hui Su <mei@hpc-login3.hpcc.usc.edu>
2018-04-10 16:20:01 -0700

    usage info

--- data/make_data_files.py
+++ data/make_data_files.py
@@ -12,7 +12,7 @@ import subprocess
 model = "CVMS5"
 
 def usage():
-    print("\n./make_data_files.py -d [cvms5] -u [uid]\n\n")
+    print("\n./make_data_files.py -d [s5] -u [uid]\n\n")
     print("-d - dataset to retrieve from hypocenter.\n")
     print("-u - username to use to do the dataset retrieval.\n")
     sys.exit(0)
