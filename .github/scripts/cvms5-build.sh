465330b351c01876c473a916549636e951fa4308
meihuisu <mei@ip-172-31-31-151.us-west-2.compute.internal>
2025-05-12 22:49:47 +0000

    fix makefile

--- .github/scripts/cvms5-build.sh
+++ .github/scripts/cvms5-build.sh
@@ -8,10 +8,11 @@ if [ $tmp == 'Darwin' ]; then
   brew reinstall gcc
 fi
 
+libtoolize
 aclocal -I m4
 automake --add-missing --force-missing
 autoconf
-./configure --prefix=$UCVM_INSTALL_PATH/model/cvms5
+./configure --prefix=$UCVM_INSTALL_PATH/model/cvms5 --with-etree-libdir=${UCVM_INSTALL_PATH}/lib/euclid3/lib --with-etree-incdir=${UCVM_INSTALL_PATH}/lib/euclid3/include --with-proj-libdir=${UCVM_INSTALL_PATH}/lib/proj/lib --with-proj-incdir=${UCVM_INSTALL_PATH}/lib/proj/include --with-tiff-libdir=${UCVM_INSTALL_PATH}/lib/tiff/lib --with-tiff-incdir=${UCVM_INSTALL_PATH}/lib/tiff/include --with-sqlite-libdir=${UCVM_INSTALL_PATH}/lib/sqlite/lib --with-sqlite-incdir=${UCVM_INSTALL_PATH}/lib/sqlite/include
 make
 make install
 
